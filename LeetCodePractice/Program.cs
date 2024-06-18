8d851e1499dd110cd4dc1e0a64300ef5d8adf164
divetri <divetrirahmawati@gmail.com>
2024-06-18 23:39:06 +0700

    odd pow

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -12,12 +12,20 @@ namespace LeetCodePractice
         {
             Console.Clear();
             Console.WriteLine("-------------");
+            Console.WriteLine("0. Odd Pow");
             Console.WriteLine("1. Two Sum");
             Console.Write("Input problem number: ");
             if (Int32.TryParse(Console.ReadLine(), out int quiz))
             {
                 switch (quiz)
                 {
+                    case 0:
+                        Console.Write("Input number to iterate: ");
+                        if (Int32.TryParse(Console.ReadLine(), out int n))
+                            Solution.OddPowCode.OddPow(n);
+                        else Console.WriteLine("Invalid input.");
+                        waitForContinue();
+                        break;
                     case 1:
                         int[] nums = {3,2,4,5,8};
                         int target = 9;
---
+++ LeetCodePractice/Solution/OddPowCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Solution
+{
+    public class OddPowCode
+    {
+        public static void OddPow(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine((i / 2) * (i / 2) + " ");
+                }
+                else
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+        }
+    }
+}
