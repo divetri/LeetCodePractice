5322a4d1ad8f4f49ed81f320ba39028f25ffe817
divetri <divetrirahmawati@gmail.com>
2024-06-18 23:14:20 +0700

    commit with vs

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -8,10 +8,42 @@ namespace LeetCodePractice
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            Console.Clear();
             Console.WriteLine("-------------");
-            Console.WriteLine("-------------");
+            Console.WriteLine("1. Two Sum");
+            Console.Write("Input problem number: ");
+            if (Int32.TryParse(Console.ReadLine(), out int quiz))
+            {
+                switch (quiz)
+                {
+                    case 1:
+                        int[] nums = {3,2,4,5,8};
+                        int target = 9;
+                        Solution.TwoSumCode.TwoSum(nums, target);
+                        waitForContinue();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input.");
+                        waitForContinue();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+                waitForContinue();
+            }
+        }
+        public static void waitForContinue()
+        {
+            Console.Write("Continue? (press Y button)");
+            string key = Console.ReadKey().Key.ToString();
+            if (key.ToUpper() == "Y")
+            {
+                Main();
+            }
         }
     }
 }
---
+++ LeetCodePractice/Solution/TwoSumCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Solution
+{
+    public class TwoSumCode
+    {
+        public static int[] TwoSumLeetCode(int[] nums, int target)
+        {
+            int[] output = new int[2];
+            for (int i = 0; i < (nums.Length / 2) + 1; i++)
+            {
+                if (nums.Contains(target - nums[i]))
+                {
+                    output[0] = i;
+                    output[1] = Array.IndexOf(nums, (target - nums[i]));
+                    continue;
+                }
+            }
+            return output;
+        }
+
+        public static void TwoSum(int[] nums, int target)
+        {
+            string output = "";
+            for (int i = 0; i < (nums.Length / 2) + 1; i++)
+            {
+                if (nums.Contains(target - nums[i]))
+                {
+                    output += i + " " + Array.IndexOf(nums, (target - nums[i]));
+                    break;
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
